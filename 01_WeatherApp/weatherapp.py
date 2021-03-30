f2cea463d8a5766c1377d52c1c4e5b29fcaef9d8
hsiec <huberts94@gmail.com>
2021-03-30 22:51:33 +0200

    Updated weatherapp

--- 01_WeatherApp/weatherapp.py
+++ 01_WeatherApp/weatherapp.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import flash, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone, timedelta
 import requests
 import sys
 
@@ -37,11 +38,11 @@ def add_city():
     if request.method == 'POST':
         city = request.form["city_name"]
         exists = Cities.query.filter_by(name=city).first()
-        if exists:
+        if exists:  # checking existance in database
             flash('The city has already been added to the list!')
 
         elif city and exists is None:
-            if check_response(city) == 200:
+            if check_response(city) == 200:  # checking response from the server
                 db.session.add(Cities(name=city))
                 db.session.commit()
             else:
@@ -60,17 +61,51 @@ def delete(city_id):
 
 
 def check_response(city):
-    url = f"{API_URL}q={city}&APPID={API_KEY}"
+    url = f"{API_URL}?q={city}&APPID={API_KEY}"
     return requests.get(url).status_code
 
 
-def get_weather(id, city, units='metric'):
-    url = f"{API_URL}q={city}&APPID={API_KEY}&units={units}"
-    req = requests.get(url)
+def get_localtime(_timezone):
+    """From passed timezone returning local time in the town"""
+    tz = timezone(timedelta(seconds=_timezone))
+    time = datetime.now(tz)
+    return time.strftime('%b %d %H:%M')
+
+
+def get_daytime(_timezone, sunrise, sunset):
+    """Returning time of the day in city"""
+    tz = timezone(timedelta(seconds=_timezone))
+    time = datetime.now(tz)
+    sun_rise = datetime.fromtimestamp(sunrise, tz)
+    sun_set = datetime.fromtimestamp(sunset, tz)
+    # hour gap is an hour after sunrise and hour before sunset
+    hour_gap = timedelta(hours=1)
+    if (sun_set - hour_gap < time < sun_set + hour_gap or
+            sun_rise - hour_gap < time < sun_rise + hour_gap):
+        return 'evening-morning'
+    elif sun_rise < time < sun_set:
+        return 'day'
+    else:
+        return 'night'
+
+
+def get_weather(_id, city, units='metric'):
+    """With City, database ID and API key returns weather dictionary"""
+    params = {'q': city, 'appid': API_KEY, 'units': units}
+    req = requests.get(API_URL, params=params)
     if req.ok:
-        data = req.json()
-        weather = {'id': id, 'city': f'{city}', 'state': data["weather"][0]["description"],
-                   'temp': round(data["main"]["temp"])}
+        data_json = req.json()
+        weather = {'id': _id,
+                   'city': f'{city}',
+                   'state': data_json["weather"][0]["description"],
+                   'temp': round(data_json["main"]["temp"]),
+                   'daytime': get_daytime(
+                       data_json["timezone"],
+                       data_json['sys']['sunrise'],
+                       data_json['sys']['sunset']
+                   ),
+                   'localtime': get_localtime(data_json["timezone"])
+                   }
         return weather
     else:
         return False
